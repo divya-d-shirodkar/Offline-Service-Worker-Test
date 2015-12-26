edc0ead83c8c4a4d8cf1c483315c25594f24e4a8
Divya Shirodkar <divya.shirodkar@klm.com>
2015-12-26 22:03:02 +0100

    console log in sw js

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
 this.addEventListener('install', function(event) {
+	console.log('install');
   event.waitUntil(
+	console.log('waituntill');
     caches.open('v1').then(function(cache) {
+		console.log('then of open cache');
       return cache.addAll([
         '/sw-test/',
         '/sw-test/index.html',
-       // '/sw-test/style.css',
         '/sw-test/app.js'        
       ]);
     })
@@ -12,12 +14,16 @@ this.addEventListener('install', function(event) {
 });
 
 this.addEventListener('fetch', function(event) {
+    console.log('fetch');
   var response;
   event.respondWith(caches.match(event.request).catch(function() {
+	console.log('respond with');  
     return fetch(event.request);
-  }).then(function(r) {
+  }).then(function(r) { 
+	console.log('fetch then=='+r); 
     response = r;
     caches.open('v1').then(function(cache) {
+	  console.log('caches open');
       cache.put(event.request, response);
     });
     return response.clone();
