5eb3e884656e0566842fab75e7693f56af028fe9
Divya Shirodkar <divya.shirodkar@klm.com>
2015-12-26 22:27:44 +0100

    hit and try

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/Offline-Service-Worker-Test/sw.js', { scope: '/Offline-Service-Worker-Test/' }).then(function(reg) {
+  navigator.serviceWorker.register('sw.js').then(function(reg) {
     // registration worked
-    console.log('Registration succeeded. Scope is ' + reg.scope);
+    console.log('Registration succeeded. Scope is ');
   }).catch(function(error) {
     // registration failed
     console.log('Registration failed with ' + error);
